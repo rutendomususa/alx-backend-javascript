e7129afd596839a033cbc20b95dc6d335822edc2
rutendomususa <157592361+rutendomususa@users.noreply.github.com>
2025-07-14 20:59:01 +0200

    node.js basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Rutendo Mususa <https://github.com/rutendomususa>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
