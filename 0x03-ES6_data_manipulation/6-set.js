f21aff1954552dc2efea4d0eb2e41cbfc69db610
rutendomususa <157592361+rutendomususa@users.noreply.github.com>
2025-07-14 21:34:28 +0200

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Rutendo Kenita Mususa <https://github.com/rutendomususa>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
